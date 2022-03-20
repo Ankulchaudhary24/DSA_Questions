28bceee5db158c9783bf4069ba2398556812ca61
Ankul Chaudhary <59824325+Ankulchaudhary24@users.noreply.github.com>
2022-03-20 12:51:35 +0530

    Create MergeKSortedArrays.cpp

---
+++ Heap/MergeKSortedArrays.cpp
@@ -0,0 +1,72 @@
+/*
+Given K sorted arrays arranged in the form of a matrix of size K*K. The task is to merge them into one sorted array.
+Example 1:
+
+Input:
+K = 3
+arr[][] = {{1,2,3},{4,5,6},{7,8,9}}
+Output: 1 2 3 4 5 6 7 8 9
+Explanation:Above test case has 3 sorted
+arrays of size 3, 3, 3
+arr[][] = [[1, 2, 3],[4, 5, 6], 
+[7, 8, 9]]
+The merged list will be 
+[1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+*/
+
+class node{
+    public:
+    int data;
+    int i;
+    int j;
+    
+    node(int data, int row, int col)
+    {
+        this->data = data;
+        i = row;
+        j = col;
+    }
+};
+
+class compare{
+    public:
+    bool operator()(node* a, node* b){
+        return a->data > b->data;
+    }
+};
+
+class Solution
+{
+    public:
+    //Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int k)
+    {
+       priority_queue<node*, vector<node*>, compare > pq;
+       
+       vector<int> ans;
+       //Step 1: insert first elements of the all the arrays
+       for(int i=0; i<k; i++)
+       {
+           node* temp = new node(arr[i][0], i, 0);
+           pq.push(temp);
+       }
+       
+       //Step 2:
+       while(pq.size() > 0)
+       {
+           node* topNode = pq.top();
+           ans.push_back(topNode->data);
+           
+           pq.pop();
+           int i = topNode->i;
+           int j = topNode->j;
+           
+           if(j+1 < arr[i].size()){
+               node* tmp = new node(arr[i][j+1],i,j+1);
+               pq.push(tmp);
+           }
+       }
+       return ans;
+    }
+};
