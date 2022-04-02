9a21e65d72171b6afc570a0df29fdccf443f4f3b
Ankul Chaudhary <59824325+Ankulchaudhary24@users.noreply.github.com>
2022-04-02 16:32:26 +0530

    Create PartitionEqualSubsetSum.cpp

---
+++ DynamicProgramming/PartitionEqualSubsetSum.cpp
@@ -0,0 +1,58 @@
+/*
+Given an array arr[] of size N, check if it can be partitioned into two parts such that the sum of elements in both parts is the same.
+
+Example 1:
+
+Input: N = 4
+arr = {1, 5, 11, 5}
+Output: YES
+Explaination: 
+The two parts are {1, 5, 5} and {11}.
+Example 2:
+
+Input: N = 3
+arr = {1, 3, 5}
+Output: NO
+Explaination: This array can never be 
+partitioned into two such parts.
+*/
+
+//Reference for the Subset sum equal to K
+bool isSubsetSum(vector<int>arr, int sum){
+    int n = arr.size();
+    int k = sum;
+    vector<bool> prev(k+1,false);
+    
+    prev[0] = true;
+    
+    if(arr[0]<=k)
+        prev[arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        vector<bool> cur(k+1,false);
+        cur[0] = true;
+        for(int target= 1; target<=k; target++){
+            bool notTaken = prev[target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = prev[target-arr[ind]];
+        
+            cur[target]= notTaken||taken;
+        }
+        prev = cur;
+    }
+    
+    return prev[k];
+ }
+
+bool canPartition(vector<int> &arr, int n)
+{
+	int sum = 0;
+    for(int i: arr){
+        sum +=i;
+    }
+     if(sum & 1 == 1)
+         return false;
+    return isSubsetSum(arr, sum/2);
+}
